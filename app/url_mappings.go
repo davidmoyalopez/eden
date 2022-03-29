ff2b47b14fc4aa7a34a07e315306b830f3955274
david.moya <david.moya@invisiblebits.com>
2022-03-29 23:14:35 +0200

    update

---
+++ app/url_mappings.go
@@ -0,0 +1,14 @@
+package app
+
+import (
+	"eden/controllers/lands"
+	"eden/controllers/ping"
+)
+
+func mapUrls() {
+	router.GET("/ping", ping.Ping)
+
+	router.GET("/lands/:lands_id", lands.Get)
+	router.GET("/lands/search", lands.Search)
+	router.POST("/lands", lands.Create)
+}
---
+++ controllers/lands/lands_controller.go
@@ -0,0 +1,17 @@
+package lands
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Create(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "implement me!")
+}
+func Get(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "implement me!")
+}
+func Search(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "implement me!")
+}
